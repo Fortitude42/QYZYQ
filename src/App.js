11f80a9e7566ce52f64692cf180c64842749a023
BaKe03 <36663387+BaKe03@users.noreply.github.com>
2022-04-28 16:57:26 +0600

    added navbar component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from "react";
 import 'bootstrap/dist/css/bootstrap.css';
 import './App.css'
+import './components/Navbar/Navbar.css'
 import { Routes, Route} from "react-router-dom";
 import Home from './components/pages/Home/Home';
+import Navbar from './components/Navbar/Navbar';
 
 
 export default class App extends Component {	
 		render() {
-			return (
-				<Routes>					
-					<Route path="/home" element={<Home />}/>
-				</Routes>
+			return(
+				<div>
+					<Navbar/>
+					<Routes>					
+						<Route path="/home" element={<Home />}/>
+					</Routes>
+				</div>
 			)
 		}
 }
